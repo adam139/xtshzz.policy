f4488eb871d8f6c194e16f05b0d630c95a6f353f
adam139 <yuejun.tang@gmail.com>
2015-02-13 22:35:48 +0800

    fixx

--- xtshzz/policy/browser/register.py
+++ xtshzz/policy/browser/register.py
@@ -1,5 +1,7 @@
 #-*- coding: UTF-8 -*-
 from five import grok
+from zope import event
+from zope.lifecycleevent import ObjectAddedEvent
 from dexterity.membrane.content.member import IOrganizationMember
 from dexterity.membrane.content.member import ISponsorMember
 from dexterity.membrane.content.memberfolder import IMemberfolder
@@ -26,15 +28,15 @@ from Products.CMFPlone import PloneMessageFactory as _p
 from xtshzz.policy.browser.interfaces import IXtshzzThemeSpecific as IThemeSpecific
 
 defaultvalue = u"""
-<h3 style="text-align: center">OWASP中国会员注册协议</h3>
-<p>注册成为OWASP中国会员，并遵守本协议条款，可以享受OWASP中国提供的服务。如果不接受本协议条款，请勿注册。接受本声明，您将遵守本协议。</p>
+<h3 style="text-align: center">湘潭市民政局社会组织促进会会员注册协议</h3>
+<p>注册成为湘潭市民政局社会组织促进会会员，并遵守本协议条款，可以享受社会组织促进会中国提供的服务。如果不接受本协议条款，请勿注册。接受本声明，您将遵守本协议。</p>
 <p><ol>
     <li>用户信息：
         <ol>
         <li>用户应自行诚信向本站提供注册资料，用户同意其提供的注册资料真实、准确、完整、合法有效，用户注册资料如有变动的，应及时更新其注册资料。如果用户提供的注册资料不合法、不真实、不准确、不详尽的，用户需承担因此引起的相应责任及后果。</li>
             <li>涉及用户真实姓名/名称、通信地址、联系电话、电子邮箱等隐私信息的，本站将予以严格保密，除非得到用户的授权或法律另有规定，本站不会向外界披露用户隐私信息。</li>
         <li>用户注册成功后，将产生用户名和密码等账户信息，您可以根据本站规定改变您的密码。用户应谨慎合理的保存、使用其用户名和密码。用户若发现任何非法使用用户账号或存在安全漏洞的情况，请立即通知本站并向公安机关报案。</li>
-        <li>用户同意，OWASP中国拥有通过邮件、短信电话等形式，向注册用户发送最新活动通知的权利。</li>
+        <li>用户同意，社会组织促进会中国拥有通过邮件、短信电话等形式，向注册用户发送最新活动通知的权利。</li>
         <li>用户不得将在本站注册获得的账户借给他人使用，否则用户应承担由此产生的全部责任，并与实际使用人承担连带责任。</li>                                    
     </ol>
         </li>
@@ -42,7 +44,7 @@ defaultvalue = u"""
         <span>用户应遵守中华人民共和国有关法律、法规和本网站有关规定。</span>
         </li>
     <li>用户权利：<br />
-        <span>用户可以获取OWASP最新资源；免费参与OWASP组织的各种沙龙、会议；获取OWASP会议演讲资源；每年免费参与OWASP组织的在线培训。</span>
+        <span>用户可以获取社会组织促进会最新资源；免费参与社会组织促进会组织的各种沙龙、会议；获取社会组织促进会会议演讲资源；每年免费参与社会组织促进会组织的在线培训。</span>
         </li>        
 </ol></p>
 """
@@ -222,6 +224,7 @@ class RegistrationSponsorForm(form.SchemaForm):
             setattr(obj, k, v)
         
         obj.reindexObject()
+        event.notify(ObjectAddedEvent(obj,self.context,data['id']))
         urltool = getToolByName(self.context, 'portal_url')
         portal = urltool.getPortalObject()
         self.request.response.redirect(portal.absolute_url() + "/login_form")
