3af194029dca5bfe3c34d015807ce421f3393ed6
adam139 <yuejun.tang@gmail.com>
2015-02-24 21:49:57 +0800

    add administrative licence id behavior

--- xtshzz/policy/behaviors/year2id.py
+++ xtshzz/policy/behaviors/year2id.py
@@ -9,12 +9,13 @@ from plone.app.content.interfaces import INameFromTitle
 class INameFromYear(Interface):
     """ Interface to adapt to INameFromTitle """
 
+class INameFromAdminYear(Interface):
+    """ Interface to adapt to INameFromTitle for administrative licence object"""
+    
 @implementer(INameFromTitle)
 @adapter(INameFromYear)
 class NameFromYear(object):
     """ Adapter to INameFromTitle """
-#    implements(INameFromTitle)
-#    adapts(INameFromYear)
 
     def __init__(self, context):
         pass
@@ -24,10 +25,25 @@ class NameFromYear(object):
         year = (datetime.datetime.today() + datetime.timedelta(-365)).strftime("%Y")    
         title = u'%s' % (year)
         inst = super(NameFromYear, cls).__new__(cls)
-
         inst.title = title
 #        context.setTitle(title)
-
         return inst
 
+@implementer(INameFromTitle)
+@adapter(INameFromAdminYear)
+class NameFromAdminYear(object):
+    """ Adapter to INameFromTitle """
 
+    def __init__(self, context):
+        pass
+
+    def __new__(cls, context):
+#        org = context.brand
+        now = datetime.datetime.today()
+        year = now.strftime("%Y")
+        day = now.strftime("%d")    
+        title = u'admin_%s_%s' % (year,day)
+        inst = super(NameFromAdminYear, cls).__new__(cls)
+        inst.title = title
+#        context.setTitle(title)
+        return inst
