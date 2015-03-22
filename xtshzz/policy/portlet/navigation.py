2555d5067fb4a1a38aba845325c2317285404639
adam139 <yuejun.tang@gmail.com>
2015-03-22 21:10:04 +0800

    add customize queryBuilder

---
+++ xtshzz/policy/portlet/navigation.py
@@ -0,0 +1,23 @@
+
+from plone.app.portlets.portlets.navigation import QueryBuilder
+from plone.app.portlets.portlets.navigation import INavigationPortlet
+from plone.app.layout.navigation.interfaces import INavigationQueryBuilder
+from zope.component import adapts
+from zope.interface import implements, Interface
+
+
+class INonDefaultPageFilteringNavigationPortlet(INavigationPortlet):
+    pass
+
+
+class DontFilterDefaultQueryBuilder(QueryBuilder):
+    implements(INavigationQueryBuilder)
+    adapts(Interface, INonDefaultPageFilteringNavigationPortlet)
+
+    def __init__(self, context, portlet):
+        super(DontFilterDefaultQueryBuilder, self).__init__(context, portlet)
+#        self.query['is_default_page'] = (True, False)  # Don't filter out default pages
+#        import pdb
+#        pdb.set_trace()
+        self.query['sort_order'] = "reverse"
+        self.query['sort_on'] = "created"
