224f016e833841441068eb11313f3985e35f53f1
adam139 <yuejun.tang@gmail.com>
2017-06-25 08:43:35 +0800

    fix homepage to render dangjian info.

--- xtshzz/policy/browser/static/homepage-custom.js
+++ xtshzz/policy/browser/static/homepage-custom.js
@@ -5,15 +5,21 @@ require([
   'use strict';
 $(document).ready(function(){ 
 	$(".nav-tabs a").mouseover(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
+		  e.preventDefault();
+		  $(this).tab('show');
 		});
 	$(".nav-tabs").on("click","a",function (e) {
 		  e.preventDefault();
 		  var url = $(this).attr("data-js-target");
 		  window.location.href = url;
 		  return false;
-		});		
+		});
+	$(".big-ad").on("click",function (e) {
+		  e.preventDefault();
+		  var url = $(this).attr("data-target");
+		  window.location.href = url;
+		  return false;
+		});				
 	StartRollV();
 	StartRollVs();
 	rolltext(".roll-wrapper");
