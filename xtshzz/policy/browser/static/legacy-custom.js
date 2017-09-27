ae58752b2a317e2f39d2255875af18b406f2accf
adam139 <yuejun.tang@gmail.com>
2017-09-27 22:30:56 +0800

    fix aotu hide colomn one rule

--- xtshzz/policy/browser/static/legacy-custom.js
+++ xtshzz/policy/browser/static/legacy-custom.js
@@ -4,7 +4,7 @@ require([
   'use strict';
 $(document).ready(function(){
 	var leftHeight = $('.portletNavigationTree dd').height();
-	var rightHeight = $('#content').height();
+	var rightHeight = $('#content').parent().height();
 	if((leftHeight) && leftHeight > rightHeight) {
 		leftHeight = rightHeight;
 		$('.portletNavigationTree dd').height(leftHeight).css("overflow","auto");
